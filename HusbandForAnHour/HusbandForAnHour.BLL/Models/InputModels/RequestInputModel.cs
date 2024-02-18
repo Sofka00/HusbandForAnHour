6da568f025344763456c4a2002419f99f1fba230
sofa <alexandro361@gmail.com>
2024-02-18 22:34:13 +0300

    Add Folder

---
+++ HusbandForAnHour/HusbandForAnHour.BLL/Models/InputModels/RequestInputModel.cs
@@ -0,0 +1,17 @@
+﻿using HusbandForAnHour.DAL.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HusbandForAnHour.BLL.Models.InpetModels
+{
+    internal class RequestInputModel
+    {
+        public int ClientId { get; set; }
+        public DateTime Date { get; set; }
+        public string Address { get; set; }
+        public string? Comment { get; set; }
+    }
+}
---
+++ HusbandForAnHour/HusbandForAnHour.BLL/ServicesClient.cs
@@ -0,0 +1,13 @@
+﻿using HusbandForAnHour.DAL;
+using HusbandForAnHour.DAL.IRepositorys;
+using HusbandForAnHour.DAL.Dtos;
+using AutoMapper;
+namespace HusbandForAnHour.BLL
+{
+    public class ServicesClient
+    {
+        private ServicesRepositiry _servicesRepositiry;
+        private Mapper _mapper;
+
+    }
+}
