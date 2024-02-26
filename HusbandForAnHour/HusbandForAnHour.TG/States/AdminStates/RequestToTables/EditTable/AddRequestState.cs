a33335ff7d1864a585eaaaed2231d270ce80b894
sofa <alexandro361@gmail.com>
2024-02-26 14:34:12 +0300

    add request

--- HusbandForAnHour/HusbandForAnHour.TG/States/AdminStates/RequestToTables/EditTable/AddRequestState.cs
+++ HusbandForAnHour/HusbandForAnHour.TG/States/AdminStates/RequestToTables/EditTable/AddRequestState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HusbandForAnHour.BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +11,32 @@ namespace HusbandForAnHour.TG.States.AdminStates.RequestToTables.EditTable
 {
     public class AddRequestState : AbstractState
     {
+        private RequestService _requestService;
+
+        public AddRequestState()
+        {
+            _requestService = new RequestService();
+        }
+
         public override AbstractState ReceiveMessage(Update update)
         {
-            throw new NotImplementedException();
+            AbstractState result = this;
+            var tmp = update.Message.Text.Split(" ");
+            if (tmp.Length>1&& tmp.Length<3)
+            {
+                _requestService.CreateRequest(update.Message.Chat.Id, DateTime.Now, tmp[0], tmp[1]);
+                result = new AdminState();
+            }
+            else
+            {
+                SingleToneStorage.GetStorage().Client.SendTextMessageAsync(update.Message.Chat.Id, "Ввод неверный");
+            }
+            return result;
         }
 
         public override void SendMessage(long chatId)
         {
-            SingleToneStorage.GetStorage().Client.SendTextMessageAsync(chatId, "Добавить заявку ");
+            SingleToneStorage.GetStorage().Client.SendTextMessageAsync(chatId, "Добавить заявку, вводите данные через пробел аддресс, комментарий");
         }
     }
 }
